00afd17d9ba99c131511eb4884a1cc0e50210713
AkramQureshi <161283057+AkramQureshi@users.noreply.github.com>
2024-03-20 22:15:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Aga Khan University Hospital");
+console.log("Paediatrics Department");
+console.log("I am working since 2002");
+var Name = "Ashaz Akram";
+var Age = "15";
+console.log(Name);
+console.log(Age);
+var num = 2024;
+console.log("my fav number is", (num));
---
+++ app.ts
@@ -0,0 +1,9 @@
+console.log("Aga Khan University Hospital")
+console.log("Paediatrics Department")
+console.log("I am working since 2002")
+let Name: string = "Akram Qureshi";
+let Age: String = "15"
+console.log(Name);
+console.log(Age);
+let num: number = 2024
+console.log("my fav number is",(num));
